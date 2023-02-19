d519d77283ef0de3244b365e0a348d5c2abf6e4c
cy.yang <cy.yang@apolitical.co>
2023-02-19 16:17:24 +0000

    Update SQL format

--- fightchurn/listings/chap7/insert_7_1_ratio_metric.sql
+++ fightchurn/listings/chap7/insert_7_1_ratio_metric.sql
@@ -4,24 +4,43 @@
 INSERT into metric_name values (%new_metric_id,concat('%new_metric_name'))
 ON CONFLICT DO NOTHING;
 
-with num_metric as (
-	select account_id, metric_time, metric_value as num_value
-	from metric m inner join metric_name n on n.metric_name_id=m.metric_name_id
-	and n.metric_name = '%num_metric'
-	and metric_time between '%from_yyyy-mm-dd' and '%to_yyyy-mm-dd'
-), den_metric as (
-	select account_id, metric_time, metric_value as den_value
-	from metric m inner join metric_name n on n.metric_name_id=m.metric_name_id
-	and n.metric_name = '%den_metric'
-	and metric_time between '%from_yyyy-mm-dd' and '%to_yyyy-mm-dd'
+WITH num_metric AS (
+			SELECT
+					account_id,
+					metric_time,
+					metric_value AS num_value
+				FROM
+					metric AS m
+	INNER JOIN metric_name AS n
+	 				ON n.metric_name_id = m.metric_name_id
+		 		 AND n.metric_name = '%num_metric'
+		 		 AND metric_time BETWEEN '%from_yyyy-mm-dd' AND '%to_yyyy-mm-dd'
+), den_metric AS (
+			SELECT
+					account_id,
+					metric_time,
+					metric_value AS den_value
+		  	FROM
+					metric AS m
+	INNER JOIN metric_name AS n
+					ON n.metric_name_id=m.metric_name_id
+			   AND n.metric_name = '%den_metric'
+				 AND metric_time BETWEEN '%from_yyyy-mm-dd' AND '%to_yyyy-mm-dd'
 )
-insert into metric (account_id,metric_time,metric_name_id,metric_value)
-select d.account_id, d.metric_time, %new_metric_id,
-	case when den_value > 0
-	    then coalesce(num_value,0.0)/den_value
-	    else 0
-    end as metric_value
-from den_metric d  left outer join num_metric n
-	on n.account_id=d.account_id
-	and n.metric_time=d.metric_time
+
+INSERT INTO metric (account_id,metric_time,metric_name_id,metric_value)
+
+				 SELECT
+						 d.account_id,
+						 d.metric_time,
+						 %new_metric_id,
+		  CASE WHEN den_value > 0
+					 THEN coalesce(num_value,0.0)/den_value
+					 ELSE 0
+					 END AS metric_value
+					 FROM den_metric AS d
+LEFT OUTER JOIN num_metric AS n
+					   ON n.account_id = d.account_id
+ 				  	AND n.metric_time = d.metric_time
+
 ON CONFLICT DO NOTHING;
