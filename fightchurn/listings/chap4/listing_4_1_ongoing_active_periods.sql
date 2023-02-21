f656a46a169bb987bab69036e81814f852c55cb2
cy.yang <cy.yang@apolitical.co>
2023-02-21 08:51:52 +0000

    Add table schema to query

--- fightchurn/listings/chap4/listing_4_1_ongoing_active_periods.sql
+++ fightchurn/listings/chap4/listing_4_1_ongoing_active_periods.sql
@@ -1,28 +1,30 @@
-
+-- 4.3.3 Finding active periods that are ongoing
 WITH RECURSIVE active_period_params AS (
     SELECT
         interval '%gap_interval' AS allowed_gap,
         '%to_yyyy-mm-dd'::date AS calc_date
 ),
 active AS (
+  -- find all accounts that are currently active
     SELECT
   DISTINCT
         account_id,
         min(start_date) AS start_date
-      FROM subscription
+      FROM socialnet7.subscription
 INNER JOIN active_period_params AS params
         ON start_date <= params.calc_date
        AND (end_date > params.calc_date OR end_date IS null)
   GROUP BY account_id
 
      UNION
-
+  -- find earlier subscriptions that overlapped with or are continuous with
+  -- but older than the subscriptions currently found
     SELECT
         subs.account_id,
         subs.start_date
-      FROM subscription AS subs
+      FROM socialnet7.subscription AS subs
 CROSS JOIN active_period_params AS params
-INNER JOIN active
+INNER JOIN active -- self, i.e. recursive join
         ON subs.account_id = active.account_id
        AND subs.start_date < active.start_date
        AND subs.end_date >= (active.start_date - params.allowed_gap)::date
