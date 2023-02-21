09b9fddcd15a6213648c6c74441ad79bf944d8e8
cy.yang <cy.yang@apolitical.co>
2023-02-21 08:26:30 +0000

    Format query

--- fightchurn/listings/chap4/listing_4_1_ongoing_active_periods.sql
+++ fightchurn/listings/chap4/listing_4_1_ongoing_active_periods.sql
@@ -1,33 +1,31 @@
-WITH RECURSIVE active_period_params AS
-(
+
+WITH RECURSIVE active_period_params AS (
     SELECT
         interval '%gap_interval' AS allowed_gap,
         '%to_yyyy-mm-dd'::date AS calc_date
 ),
-active AS
-(
+active AS (
     SELECT
   DISTINCT
         account_id,
         min(start_date) AS start_date
       FROM subscription
-INNER JOIN active_period_params
-        ON start_date <= calc_date
-       AND (end_date > calc_date OR end_date IS null)
+INNER JOIN active_period_params AS params
+        ON start_date <= params.calc_date
+       AND (end_date > params.calc_date OR end_date IS null)
   GROUP BY account_id
 
-    UNION
+     UNION
 
     SELECT
-        s.account_id,
-        s.start_date
-      FROM subscription s
-CROSS JOIN active_period_params
-INNER JOIN active e
-        ON s.account_id=e.account_id
-       AND s.start_date < e.start_date
-       AND s.end_date >= (e.start_date-allowed_gap)::date
-
+        subs.account_id,
+        subs.start_date
+      FROM subscription AS subs
+CROSS JOIN active_period_params AS params
+INNER JOIN active
+        ON subs.account_id = active.account_id
+       AND subs.start_date < active.start_date
+       AND subs.end_date >= (active.start_date - params.allowed_gap)::date
 )
 
 INSERT INTO active_period (account_id, start_date, churn_date)
