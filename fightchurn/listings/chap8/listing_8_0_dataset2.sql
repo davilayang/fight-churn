e29325a0cce4962b4de1c1d3cd140d2eeb128b47
cy.yang <cy.yang@apolitical.co>
2023-03-15 23:02:54 +0000

    Format queries

--- fightchurn/listings/chap8/listing_8_0_dataset2.sql
+++ fightchurn/listings/chap8/listing_8_0_dataset2.sql
@@ -1,30 +1,42 @@
-with observation_params as
-(
-    select  interval '%metric_interval' as metric_period,
-    '%from_yyyy-mm-dd'::timestamp as obs_start,
-    '%to_yyyy-mm-dd'::timestamp as obs_end
+-- listing_8_0_dataset2.sql
+
+WITH observation_params AS (
+    SELECT  
+        interval '%metric_interval' AS metric_period,
+        '%from_yyyy-mm-dd'::timestamp AS obs_start,
+        '%to_yyyy-mm-dd'::timestamp AS obs_end
 )
-select m.account_id, o.observation_date, is_churn,
-sum(case when metric_name_id=0 then metric_value else 0 end) as like_per_month,
-sum(case when metric_name_id=1 then metric_value else 0 end) as newfriend_per_month,
-sum(case when metric_name_id=2 then metric_value else 0 end) as post_per_month,
-sum(case when metric_name_id=3 then metric_value else 0 end) as adview_per_month,
-sum(case when metric_name_id=4 then metric_value else 0 end) as dislike_per_month,
-sum(case when metric_name_id=34 then metric_value else 0 end) as unfriend_per_month,
-sum(case when metric_name_id=6 then metric_value else 0 end) as message_per_month,
-sum(case when metric_name_id=7 then metric_value else 0 end) as reply_per_month,
-sum(case when metric_name_id=21 then metric_value else 0 end) as adview_per_post,
-sum(case when metric_name_id=22 then metric_value else 0 end) as reply_per_message,
-sum(case when metric_name_id=23 then metric_value else 0 end) as like_per_post,
-sum(case when metric_name_id=24 then metric_value else 0 end) as post_per_message,
-sum(case when metric_name_id=28 then metric_value else 0 end) as unfriend_per_newfriend,
-sum(case when metric_name_id=27 then metric_value else 0 end) as dislike_pcnt,
-sum(case when metric_name_id=30 then metric_value else 0 end) as newfriend_pcnt_chng,
-sum(case when metric_name_id=31 then metric_value else 0 end) as days_since_newfriend
-from metric m inner join observation_params
-on metric_time between obs_start and obs_end
-inner join observation o on m.account_id = o.account_id
-    and m.metric_time > (o.observation_date - metric_period)::timestamp
-    and m.metric_time <= o.observation_date::timestamp
-group by m.account_id, metric_time, observation_date, is_churn
-order by observation_date,m.account_id
+
+    SELECT 
+        m.account_id, 
+        o.observation_date, 
+        is_churn,
+        SUM(CASE WHEN metric_name_id=0 THEN metric_value ELSE 0 END) AS like_per_month,
+        SUM(CASE WHEN metric_name_id=1 THEN metric_value ELSE 0 END) AS newfriend_per_month,
+        SUM(CASE WHEN metric_name_id=2 THEN metric_value ELSE 0 END) AS post_per_month,
+        SUM(CASE WHEN metric_name_id=3 THEN metric_value ELSE 0 END) AS adview_per_month,
+        SUM(CASE WHEN metric_name_id=4 THEN metric_value ELSE 0 END) AS dislike_per_month,
+        SUM(CASE WHEN metric_name_id=6 THEN metric_value ELSE 0 END) AS message_per_month,
+        SUM(CASE WHEN metric_name_id=7 THEN metric_value ELSE 0 END) AS reply_per_month,
+        SUM(CASE WHEN metric_name_id=21 THEN metric_value ELSE 0 END) AS adview_per_post,
+        SUM(CASE WHEN metric_name_id=22 THEN metric_value ELSE 0 END) AS reply_per_message,
+        SUM(CASE WHEN metric_name_id=23 THEN metric_value ELSE 0 END) AS like_per_post,
+        SUM(CASE WHEN metric_name_id=24 THEN metric_value ELSE 0 END) AS post_per_message,
+        SUM(CASE WHEN metric_name_id=27 THEN metric_value ELSE 0 END) AS dislike_pcnt,
+        SUM(CASE WHEN metric_name_id=28 THEN metric_value ELSE 0 END) AS unfriend_per_newfriend,
+        SUM(CASE WHEN metric_name_id=30 THEN metric_value ELSE 0 END) AS newfriend_pcnt_chng,
+        SUM(CASE WHEN metric_name_id=31 THEN metric_value ELSE 0 END) AS days_since_newfriend,
+        SUM(CASE WHEN metric_name_id=34 THEN metric_value ELSE 0 END) AS unfriend_per_month
+      FROM 
+        metric AS m 
+INNER JOIN 
+        observation_params
+        ON metric_time BETWEEN obs_start AND obs_end
+INNER JOIN observation AS o 
+        ON m.account_id = o.account_id
+       AND m.metric_time > (o.observation_date - metric_period)::timestamp
+       AND m.metric_time <= o.observation_date::timestamp
+  GROUP BY 
+        m.account_id, metric_time, observation_date, is_churn
+  ORDER BY 
+        observation_date, m.account_id
--- fightchurn/listings/chap8/listing_8_3_dataset2_current.sql
+++ fightchurn/listings/chap8/listing_8_3_dataset2_current.sql
@@ -1,34 +1,51 @@
-with metric_date as
-(
-    select  max(metric_time) as last_metric_time from metric
-),
-account_tenures as (
-    select account_id, metric_value as account_tenure
-    from metric m inner join metric_date on metric_time =last_metric_time
-    where metric_name_id = 8
-    and metric_value >= 14
+-- listing_8_3_dataset2_current
+
+WITH metric_date AS (
+    SELECT 
+        MAX(metric_time) AS last_metric_time 
+      FROM metric
+), 
+account_tenures AS (
+    SELECT 
+        account_id, 
+        metric_value AS account_tenure
+      FROM metric AS m 
+INNER JOIN metric_date 
+        ON metric_time = last_metric_time
+    WHERE metric_name_id = 8
+      AND metric_value >= 14
 )
-select s.account_id, d.last_metric_time as observation_date,
-sum(case when metric_name_id=0 then metric_value else 0 end) as like_per_month,
-sum(case when metric_name_id=1 then metric_value else 0 end) as newfriend_per_month,
-sum(case when metric_name_id=2 then metric_value else 0 end) as post_per_month,
-sum(case when metric_name_id=3 then metric_value else 0 end) as adview_per_month,
-sum(case when metric_name_id=4 then metric_value else 0 end) as dislike_per_month,
-sum(case when metric_name_id=34 then metric_value else 0 end) as unfriend_per_month,
-sum(case when metric_name_id=6 then metric_value else 0 end) as message_per_month,
-sum(case when metric_name_id=7 then metric_value else 0 end) as reply_per_month,
-sum(case when metric_name_id=21 then metric_value else 0 end) as adview_per_post,
-sum(case when metric_name_id=22 then metric_value else 0 end) as reply_per_message,
-sum(case when metric_name_id=23 then metric_value else 0 end) as like_per_post,
-sum(case when metric_name_id=24 then metric_value else 0 end) as post_per_message,
-sum(case when metric_name_id=25 then metric_value else 0 end) as unfriend_per_newfriend,
-sum(case when metric_name_id=27 then metric_value else 0 end) as dislike_pcnt,
-sum(case when metric_name_id=30 then metric_value else 0 end) as newfriend_pcnt_chng,
-sum(case when metric_name_id=31 then metric_value else 0 end) as days_since_newfriend
-from metric m inner join metric_date d on m.metric_time = d.last_metric_time
-inner join account_tenures a on a.account_id = m.account_id
-inner join subscription s on m.account_id=s.account_id
-where s.start_date <= d.last_metric_time
-and (s.end_date >= d.last_metric_time or s.end_date is null)
-group by s.account_id, d.last_metric_time
-order by s.account_id
+
+    SELECT 
+        s.account_id, 
+        d.last_metric_time AS observation_date,
+        SUM(CASE WHEN metric_name_id=0 THEN metric_value ELSE 0 END) AS like_per_month,
+        SUM(CASE WHEN metric_name_id=1 THEN metric_value ELSE 0 END) AS newfriend_per_month,
+        SUM(CASE WHEN metric_name_id=2 THEN metric_value ELSE 0 END) AS post_per_month,
+        SUM(CASE WHEN metric_name_id=3 THEN metric_value ELSE 0 END) AS adview_per_month,
+        SUM(CASE WHEN metric_name_id=4 THEN metric_value ELSE 0 END) AS dislike_per_month,
+        SUM(CASE WHEN metric_name_id=6 THEN metric_value ELSE 0 END) AS message_per_month,
+        SUM(CASE WHEN metric_name_id=7 THEN metric_value ELSE 0 END) AS reply_per_month,
+        SUM(CASE WHEN metric_name_id=21 THEN metric_value ELSE 0 END) AS adview_per_post,
+        SUM(CASE WHEN metric_name_id=22 THEN metric_value ELSE 0 END) AS reply_per_message,
+        SUM(CASE WHEN metric_name_id=23 THEN metric_value ELSE 0 END) AS like_per_post,
+        SUM(CASE WHEN metric_name_id=24 THEN metric_value ELSE 0 END) AS post_per_message,
+        SUM(CASE WHEN metric_name_id=25 THEN metric_value ELSE 0 END) AS unfriend_per_newfriend,
+        SUM(CASE WHEN metric_name_id=27 THEN metric_value ELSE 0 END) AS dislike_pcnt,
+        SUM(CASE WHEN metric_name_id=30 THEN metric_value ELSE 0 END) AS newfriend_pcnt_chng,
+        SUM(CASE WHEN metric_name_id=31 THEN metric_value ELSE 0 END) AS days_since_newfriend,
+        SUM(CASE WHEN metric_name_id=34 THEN metric_value ELSE 0 END) AS unfriend_per_month
+      FROM 
+        metric AS m 
+INNER JOIN metric_date AS d 
+        ON m.metric_time = d.last_metric_time
+INNER JOIN account_tenures AS a 
+        ON a.account_id = m.account_id
+INNER JOIN subscription AS s 
+        ON m.account_id = s.account_id
+     WHERE s.start_date <= d.last_metric_time
+       AND (s.end_date >= d.last_metric_time OR s.end_date IS null)
+  GROUP BY 
+        s.account_id, d.last_metric_time
+  ORDER BY 
+        s.account_id
